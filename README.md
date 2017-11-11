33fa7c3dddb72f372a84eb423f71e89e4b518851
IAmAGit <wangyunfeiafei@outlook.com>
2017-11-11 11:09:54 +0800

    修改文档

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# this is my project
+# js排序算法
+语言：Javascript  
+作者：王云飞  
+简述：以JavaScript实现排序算法  
+基类：sort.js  
+各大排序算法：  
++ [选择排序](./selectionsort/selectionSort.js)
+
--- shellSort/shellSort.js
+++ shellSort/shellSort.js
@@ -13,6 +13,9 @@ shellsort.prototype.sort = function(arr){
         h = h*3+1;
     }
     while(h>=1){
+        //当h降到1时，就是一个插入排序，
+        //h》1时的操作是将数组变得近乎有序
+        //近乎有序的数组插入排序的效率将会非常高
         for(let i = h;i<n;i++){
             for(let j = i;j>=h&&arr[j]<arr[j-h];j-=h){
                 this.exch(arr,j,j-h);
