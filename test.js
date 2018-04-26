1873d1fe8bdf5561bdcc449b82d09331652ef4f2
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-04-26 17:58:31 +0800

    增加结果文件

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const heapSort = require('./heapSort/heapsort1')
 const quickSort = require('./quickSort/quicksort3')
 const mergeSort = require('./mergeSort/mergeSort4')
@@ -5,6 +6,8 @@ const mergeSort = require('./mergeSort/mergeSort4')
 const hs = new heapSort()
 const qs = new quickSort()
 const ms = new mergeSort()
+
+let result = {}
 
 for(let n=1000;n<=1000000;n*=10) {
     let arr = hs.generateRandomArray(n,0,n);
@@ -17,4 +20,15 @@ for(let n=1000;n<=1000000;n*=10) {
     qs.speedTest(qs,arr2);
     console.log("归并排序");
     ms.speedTest(ms,arr3);
+
+    result[`${n}`] = arr;
 }
+
+fs.exists('./result.json',(exists) => {
+    if(!exists) {
+        fs.writeFile('result.json',JSON.stringify(result),(err) => {
+            if(err)throw err;
+            console.log('result saved');
+        })
+    }
+})
