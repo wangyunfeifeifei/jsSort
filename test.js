82877300d52bc8489c85a59e5bddf152e7160ca8
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-05-07 12:45:36 +0800

    update

--- test.js
+++ test.js
@@ -7,8 +7,9 @@ const hs = new heapSort()
 const qs = new quickSort()
 const ms = new mergeSort()
 
-let result = {}
-
+console.log(`-----------------  随机数组 ---------------------
+-----------------------------------------------------------------
+-----------------------------------------------------------------`)
 for(let n=1000;n<=1000000;n*=10) {
     let arr = hs.generateRandomArray(n,0,n);
     let arr2 = hs.copyArr(arr);
@@ -20,15 +21,44 @@ for(let n=1000;n<=1000000;n*=10) {
     qs.speedTest(qs,arr2);
     console.log("归并排序");
     ms.speedTest(ms,arr3);
-
-    result[`${n}`] = arr;
 }
 
-fs.exists('./result.json',(exists) => {
-    if(!exists) {
-        fs.writeFile('result.json',JSON.stringify(result),(err) => {
-            if(err)throw err;
-            console.log('result saved');
-        })
+console.log(`-----------------  升序数组 ---------------------
+-----------------------------------------------------------------
+-----------------------------------------------------------------`)
+for(let n=1000;n<=1000000;n*=10) {
+    let arr = [];
+    for(let i=0;i<n;i++) {
+        arr[i] = i;
     }
-})
+    let arr2 = hs.copyArr(arr);
+    let arr3 = hs.copyArr(arr);
+    console.log(`--------------------n=${n}:-----------------------`)
+    console.log("堆排序:")
+    hs.speedTest(hs,arr);
+    console.log("快速排序");
+    qs.speedTest(qs,arr2);
+    console.log("归并排序");
+    ms.speedTest(ms,arr3);
+}
+
+console.log(`-----------------  降序数组 ---------------------
+-----------------------------------------------------------------
+-----------------------------------------------------------------`)
+for(let n=1000;n<=1000000;n*=10) {
+    let arr = [];
+    for(let i=n-1;i>=0;i--) {
+        arr[i] = i;
+    }
+    let arr2 = hs.copyArr(arr);
+    let arr3 = hs.copyArr(arr);
+    console.log(`--------------------n=${n}:-----------------------`)
+    console.log("堆排序:")
+    hs.speedTest(hs,arr);
+    console.log("快速排序");
+    qs.speedTest(qs,arr2);
+    console.log("归并排序");
+    ms.speedTest(ms,arr3);
+}
+
+
